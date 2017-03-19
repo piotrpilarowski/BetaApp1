7ad12576a3d62d06163d1b5205f8ac0eea582037
Piotr Pilarowski <piotr.pilarowski@outlook.com>
2017-03-19 01:36:19 +0100

    add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+
+	def full_title(page_title = '')
+
+		base_title = "Micro-messeging application"
+
+		if page_title.empty?
+
+			base_title
+
+		else
+
+			page_title + " | " + base_title
+
+		end
+	end
+
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -8,7 +8,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
 
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
     assert_select "title", "Home | #{@base_title}"
   end
