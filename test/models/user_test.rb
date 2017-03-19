93dc9770b421239c49ce43f0c604a5cb051af0e2
piotrpilarowski <piotr.pilarowski@outlook.com>
2017-03-19 20:32:22 +0100

    Updates formatting

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -56,7 +56,7 @@ class UserTest < ActiveSupport::TestCase
 
 		valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
     		valid_addresses.each do |valid_address|
-      		@user.email = valid_address
+      			@user.email = valid_address
       		assert @user.valid?, "#{valid_address.inspect} should be valid"
       		end
     	end
@@ -65,7 +65,7 @@ class UserTest < ActiveSupport::TestCase
 
     		invalid_addresses = %w[user@example,com user_at_foo.org user.name@example. foo@bar_baz.com foo@bar+baz.com foo@bar..com]
     		invalid_addresses.each do |invalid_address|
-      		@user.email = invalid_address
+      			@user.email = invalid_address
       		assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
     		end
   	end
