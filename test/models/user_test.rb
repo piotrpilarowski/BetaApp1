f7569b98d85f215c34c6e671a210a2f5ae3889ca
Piotr Pilarowski <piotr.pilarowski@outlook.com>
2017-03-20 15:44:11 +0100

    finish user signup

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -78,7 +78,7 @@ class UserTest < ActiveSupport::TestCase
   	end
 
   	test "email addresses should be saved as lower-case" do
-    	mixed_case_email = "Foo@ExAMPle.CoM"
+  		mixed_case_email = "Foo@ExAMPle.CoM"
     	@user.email = mixed_case_email
     	@user.save
     	assert_equal mixed_case_email.downcase, @user.reload.email
